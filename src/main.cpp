77c09619a79aef33cc9a8e7b5ebc70ab415788d5
TimoRaab <Timo.Raab@uni-konstanz.de>
2024-04-06 19:04:56 +0200

    Schematic extended. Rudimentary Software done. Holding Button is not working so far

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,164 @@
 #include <Arduino.h>
 #include <Esp.h>
+#include <Ticker.h>
+#include <ButtonDebounce.h>
 
 
-//TODO Interrupt
-//TODO PWM
+#define FIRSTPRESS 0
+#define LONGPRESSFIRST 1
+#define LONGPRESSSECOND 2
+
+#define WAITTIMEFIRST 1000
+#define WAITTIMESECOND 500
+
+
+void IRAM_ATTR updateButtons();
+void switchOnOff();
+void lumIncrease();
+void lumDecrease();
+
+Ticker buttonReader;
+ButtonDebounce b_onOff = ButtonDebounce(14, true, false, *switchOnOff);
+ButtonDebounce b_decrease = ButtonDebounce(13, true, false, *lumDecrease);
+ButtonDebounce b_increase = ButtonDebounce(12, true, false, *lumIncrease);
+
 
 
 const int ledPin = 16; 
 
+bool onOff = true;
+uint8 b_onOff_State = FIRSTPRESS;
+uint8 b_decrease_State = FIRSTPRESS;
+uint8 b_increase_State = FIRSTPRESS;
+uint32 b_decrease_time = 0;
+uint32 b_increase_time = 0;
+
+uint8 analogDuty = 125;
+
 void setup() {
-  
+  analogWriteFreq(4000);
+
+  buttonReader.attach_ms(15, updateButtons);
+  onOff = true;
+  analogWrite(ledPin, analogDuty);
 }
 
 void loop() {
-  // increase the LED brightness
-  for(int dutyCycle = 0; dutyCycle < 255; dutyCycle++){   
-    // changing the LED brightness with PWM
-    analogWrite(ledPin, dutyCycle);
-    delay(1);
+  if (!b_onOff.anyPressed(true)) {
+    b_onOff_State = FIRSTPRESS;
+  }
+  if (!b_decrease.anyPressed(true)) {
+    b_decrease_State = FIRSTPRESS;
+  }
+  if (!b_increase.anyPressed(true)) {
+    b_increase_State = FIRSTPRESS;
+  }
+}
+
+
+
+void IRAM_ATTR updateButtons() {
+  b_onOff.updateButton();
+  b_decrease.updateButton();
+  b_increase.updateButton();
+}
+
+
+void switchOnOff() {
+  if (b_onOff_State == LONGPRESSFIRST) return;
+  b_onOff_State = LONGPRESSFIRST;
+  uint8 tempAnalog = analogDuty;
+  if (onOff) {
+    tempAnalog = 0;
+  } 
+  onOff = !onOff;
+  analogWrite(ledPin, tempAnalog);
+}
+
+
+void lumIncrease() {
+  if (!onOff) {
+    switchOnOff();
+    return;
+  }
+  if (analogDuty == 255) return;
+  if (b_increase_State == FIRSTPRESS) {
+    if (analogDuty == 0) {
+      analogDuty = 15;
+    } else {
+      analogDuty += 10;
+    }
+    analogWrite(ledPin, analogDuty);
+    b_increase_time = millis();
+    b_increase_State = LONGPRESSFIRST;
+    return;
+  }
+  
+  if (b_increase_State == LONGPRESSFIRST) {
+    if (millis() > (b_increase_time + WAITTIMEFIRST)) {
+      analogDuty += 10;
+      analogWrite(ledPin, analogDuty);
+      b_increase_time = millis();
+      b_increase_State = LONGPRESSSECOND;
+      return;
+    } 
   }
 
-  // decrease the LED brightness
-  for(int dutyCycle = 255; dutyCycle > 0; dutyCycle--){
-    // changing the LED brightness with PWM
-    analogWrite(ledPin, dutyCycle);
-    delay(1);
+  if (b_increase_State == LONGPRESSFIRST) {
+    if (millis() > (b_increase_time + WAITTIMESECOND)) {
+      analogDuty += 10;
+      analogWrite(ledPin, analogDuty);
+      b_increase_time = millis();
+      b_increase_State = LONGPRESSSECOND;
+      return;
+    } 
+  }
+}
+
+
+void lumDecrease() {
+  if (!onOff) {
+    switchOnOff();
+    return;
+  }
+  if (analogDuty == 0) return;
+  if (b_decrease_State == FIRSTPRESS) {
+    if (analogDuty == 15) {
+      analogDuty = 0;
+    } else {
+      analogDuty -= 10;
+    }
+    analogWrite(ledPin, analogDuty);
+    b_decrease_time = millis();
+    b_decrease_State = LONGPRESSFIRST;
+    return;
+  }
+  
+  if (b_decrease_State == LONGPRESSFIRST) {
+    if (millis() > (b_decrease_time + WAITTIMEFIRST)) {
+      if (analogDuty == 15) {
+        analogDuty = 0;
+      } else {
+        analogDuty -= 10;
+      }
+      analogWrite(ledPin, analogDuty);
+      b_decrease_time = millis();
+      b_decrease_State = LONGPRESSSECOND;
+      return;
+    } 
+  }
+
+  if (b_decrease_State == LONGPRESSFIRST) {
+    if (millis() > (b_decrease_time + WAITTIMESECOND)) {
+      if (analogDuty == 15) {
+        analogDuty = 0;
+      } else {
+        analogDuty -= 10;
+      }
+      analogWrite(ledPin, analogDuty);
+      b_decrease_time = millis();
+      b_decrease_State = LONGPRESSSECOND;
+      return;
+    } 
   }
 }
